2e105d2348fd172a376debfed0fe14c72a55afe8
Pablo Banzo <pridapablo@gmail.com>
2023-11-29 22:38:25 -0600

    dual server

--- Server/server.py
+++ Server/server.py
@@ -1,8 +1,131 @@
-from agent import *
+from flask import Flask, request, jsonify
 from model import CityModel
-from mesa.visualization import CanvasGrid, BarChartModule
-from mesa.visualization import ModularServer
+from agent import Car, Traffic_Light, Obstacle, Road, Destination
+import argparse
+import threading
+import time
+import requests
+from mesa.visualization import CanvasGrid, ModularServer
 
+# Model configuration
+width = 0
+height = 0
+cityModel = None
+currentStep = 0
+
+app = Flask("Traffic")
+
+def background_task(post_endpoint, periodicity):
+    print(f"Starting background task to post to {post_endpoint} every {periodicity} seconds.")
+    global cityModel
+    while True:
+        try:
+            if cityModel:
+                car_count = cityModel.get_car_count()
+                total_trips = cityModel.get_complete_trips()
+                payload = {
+                    "year": 2023,
+                    "classroom": 301,
+                    "name": "Equipo 2: Swifties",
+                    "num_cars": car_count,
+                    "num_trips": total_trips,
+                }
+                print(f"Payload: {payload}")
+                response = requests.post(post_endpoint, json=payload)
+                print(f"Posted to {post_endpoint}: {response.status_code} {response.reason}")
+        except Exception as e:
+            print(f"Error during POST: {e}")
+        time.sleep(periodicity)
+
+@app.route('/init', methods=['POST']) #
+def initModel():
+    global width, height, cityModel, currentStep
+    if request.method == 'POST':
+        currentStep = 0
+
+        cityModel = CityModel()
+        return jsonify({"message":"Parameters recieved, model initiated."})
+    else:
+        return jsonify({
+            "message": "Method not allowed."
+        }), 405
+    
+@app.route('/getAgents', methods=['GET'])
+def getAgents():
+    global cityModel
+    if not cityModel:
+        return jsonify({
+            "message": "Model not initialized."
+        }), 500
+
+    # Retrieve the 'static' parameter from the URL, default to False if not provided
+    # static = request.args.get('static', 'false').lower() == 'true'
+
+    if request.method == 'GET':
+        obstaclePositions = [{"id": str(obstacle.unique_id), "x": x, "y": 0, "z": z}
+                    for x in range(cityModel.grid.width)
+                    for z in range(cityModel.grid.height)
+                    for obstacle in cityModel.grid.get_cell_list_contents((x, z))
+                    if isinstance(obstacle, Obstacle)]
+        trafficLightPositions = [{"id": str(a.unique_id), "x": x, "y": 0, "z": z, "state": "red" if not a.state else "green", "axis": a.axis, "direction": a.direction}
+                            for x in range(cityModel.grid.width)
+                            for z in range(cityModel.grid.height)
+                            for a in cityModel.grid.get_cell_list_contents((x, z))
+                            if isinstance(a, Traffic_Light)]
+        roadPositions = [{"id": str(road.unique_id), "x": x, "y": 0, "z": z}
+                    for x in range(cityModel.grid.width)
+                    for z in range(cityModel.grid.height)
+                    for road in cityModel.grid.get_cell_list_contents((x, z))
+                    if isinstance(road, Road)]
+        destinationPositions = [{"id": str(destination.unique_id), "x": x, "y": 0, "z": z}
+                    for x in range(cityModel.grid.width)
+                    for z in range(cityModel.grid.height)
+                    for destination in cityModel.grid.get_cell_list_contents((x, z))
+                    if isinstance(destination, Destination)]
+        carPositions = [{"id": str(car.unique_id), "x": x, "y": 0, "z": z}
+                        for x in range(cityModel.grid.width)
+                        for z in range(cityModel.grid.height)
+                        for car in cityModel.grid.get_cell_list_contents((x, z))
+                        if isinstance(car, Car)]
+        
+        print(f"Red traffic lights: {len([tl for tl in cityModel.traffic_lights if tl.state == False])}")
+        
+        return jsonify(
+            {'carPos':carPositions,
+                'obstaclePos':obstaclePositions, 
+                'trafficLightPos':trafficLightPositions, 
+                'roadPos':roadPositions, 
+                'destinationPos':destinationPositions
+                })
+
+@app.route('/update', methods=['GET'])
+def updateModel():
+    # desde unity se va a mandar un get para que se actualice el modelo
+    # una vez que se actualice, se regresa un mensaje de que se actualizó y el
+    # paso en el que va
+    global currentStep, cityModel
+    if not cityModel:
+        return jsonify({
+            "message": "Model not initialized."
+        }), 500
+    if request.method == 'GET':
+        cityModel.step()
+        currentStep += 1
+        return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
+
+@app.route('/setCycle', methods=['POST'])
+def updateCycle():
+    global cityModel
+    if not cityModel:
+        return jsonify({
+            "message": "Model not initialized."
+        }), 500
+    if request.method == 'POST':
+        cityModel.set_cycle(request.json['cycle'])
+        return jsonify({'message':f'Cycle updated to {cityModel.cycle}.'})
+
+
+# 2D visualization
 def agent_portrayal(agent):
     if agent is None: return
     
@@ -42,18 +165,75 @@ def agent_portrayal(agent):
 
     return portrayal
 
+# Global variables for model dimensions
 width = 0
 height = 0
 
+# Read city file and determine dimensions
 with open('city_files/2023_base.txt') as baseFile:
     lines = baseFile.readlines()
-    width = len(lines[0])-1
+    width = len(lines[0]) - 1
     height = len(lines)
 
-
-print(width, height)
 grid = CanvasGrid(agent_portrayal, width, height, 500, 500)
+mesa_server = ModularServer(CityModel, [grid], "Traffic Base")
 
-server = ModularServer(CityModel, [grid], "Traffic Base")          
-server.port = 8521 # The default
-server.launch()
+# Argument validation functions
+def validate_port(port):
+    """Validate the port number is within the acceptable range."""
+    port = int(port)
+    if port < 1024 or port > 65535:
+        raise argparse.ArgumentTypeError("Port number should be between 1024 and 65535.")
+    return port
+
+def validate_post_periodicity(periodicity):
+    """Validate the post periodicity is a positive integer."""
+    periodicity = int(periodicity)
+    if periodicity <= 0:
+        raise argparse.ArgumentTypeError("Post periodicity should be a positive integer.")
+    return periodicity
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Traffic model server configuration.')
+
+    # Server configuration
+    server_group = parser.add_argument_group('server configuration')
+    server_group.add_argument('-p', '--port', type=int, default=8585,
+                            help='Port number for the server to listen on. Default is 8585.')
+
+    # Stats posting configuration
+    post_group = parser.add_argument_group('stats posting configuration')
+    post_group.add_argument('-e', '--endpoint', type=str, 
+                            default="http://52.1.3.19:8585/api/validate_attempt",
+                            help='Endpoint URL for posting data. Default is http://52.1.3.19:8585/api/validate_attempt.')
+    post_group.add_argument('-f', '--frequency', type=int, default=60,
+                            help='Time interval (in seconds) between consecutive posts. Default is 60 seconds.')
+
+    # Mode configuration
+    mode_group = parser.add_argument_group('mode configuration')
+    mode_group.add_argument('-m', '--mode', choices=['2d', '3d'], default='3d',
+                            help='Visualization mode: 2d mesa portrayal or 3d (for use with Unity). Default is 3d. '
+                                'Note: The server will not ping the post-endpoint in 2d mode.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Start the background task for posting data, only in 3d mode
+    if args.mode == '3d' and args.endpoint:
+        thread = threading.Thread(
+            target=background_task, 
+            args=(args.endpoint, args.frequency), 
+            daemon=True
+        )
+        thread.start()
+
+    # Launch the appropriate server based on the mode
+    if args.mode == '2d':
+        mesa_server.port = args.port
+        mesa_server.launch()
+    else:
+        app.run(
+            host='localhost',
+            port=args.port,
+            debug=True
+        )
